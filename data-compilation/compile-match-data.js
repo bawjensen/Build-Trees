2e9df30c38884da5dccf474b37dd960bda00409d
bawjensen <bryanawjensen@gmail.com>
2015-08-18 15:14:45 -0400

    Lowering rate limit

--- data-compilation/compile-match-data.js
+++ data-compilation/compile-match-data.js
@@ -4,7 +4,7 @@ var promises    = require('../helpers/promised.js'),
 // --------------------------------------- Global Variables -------------------------------------
 
 var API_KEY             = process.env.RIOT_KEY;
-var DEFAULT_RATE_LIMIT  = 1000;
+var DEFAULT_RATE_LIMIT  = 250;
 var RATE_LIMIT          = DEFAULT_RATE_LIMIT;
 var MATCH_LIMIT         = process.argv[2] ? parseInt(process.argv[2]) : 10000;
 
