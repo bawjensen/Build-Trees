d475ebb6bee788d663fc738bfb1d923aabdda1a0
Bryan Jensen <bryanawjensen@gmail.com>
2015-08-18 10:18:33 -0700

    Got it working, but it's running request batches in parallel, maybe bad

--- data-compilation/compile-match-data.js
+++ data-compilation/compile-match-data.js
@@ -14,7 +14,9 @@ var MODE = process.argv[3] ?
 
 console.log('In mode:', MODE);
 
-var matchEndpoint       = 'https://na.api.pvp.net/api/lol/na/v2.2/match/';
+var endpointPrefix      = 'https://'
+var apiUrl              = '.api.pvp.net/api/lol/'
+var matchEndpoint       = '/v2.2/match/';
 
 var matchOptions = {
     'includeTimeline': 'true',
@@ -34,53 +36,64 @@ function fetchAndStore() {
     var desiredFrameData = new Set([ 'events' ]);
     var desiredParticipantData = new Set([ 'championId', 'participantId' ]);
 
+    var allDesiredData = [
+        { filePrefix: 'NA', regionStr: 'na' },
+        { filePrefix: 'LAN', regionStr: 'lan' }
+    ];
+
     return promises.openDB('mongodb://localhost:27017/lol-data')
         .then(function(newDB) { db = newDB; })
-        .then(promises.read.bind(null, 'json-data/matches/' + (MODE === 'After' ? '5.14' : '5.11') + '/RANKED_SOLO/NA.json'))
-        .then(JSON.parse)
-        .then(function fetchEverything(matches) {
-            return promises.rateLimitedGet(matches.slice(0, MATCH_LIMIT), RATE_LIMIT,
-                function(matchId) {
-                    return promises.persistentGet(matchEndpoint + matchId + matchQuery);
-                },
-                function(matchData) {
-                    matchData.timeline.frames = matchData.timeline.frames.filter(function(frame) {
-                        if (!frame.events) return false;
+        .then(function() {
+            return Promise.all(
+                allDesiredData.map(function(regionObj) {
+                    let regionEndpoint = endpointPrefix + regionObj.regionStr + apiUrl + regionObj.regionStr + matchEndpoint;
+                    return promises.read('json-data/matches/' + (MODE === 'After' ? '5.14' : '5.11') + '/RANKED_SOLO/' + regionObj.filePrefix + '.json')
+                        .then(JSON.parse)
+                        .then(function fetchEverything(matches) {
+                            return promises.rateLimitedGet(matches.slice(0, MATCH_LIMIT), RATE_LIMIT,
+                                function(matchId) {
+                                    return promises.persistentGet(regionEndpoint + matchId + matchQuery);
+                                },
+                                function(matchData) {
+                                    matchData.timeline.frames = matchData.timeline.frames.filter(function(frame) {
+                                        if (!frame.events) return false;
 
-                        for (var key in frame) {
-                            if (!desiredFrameData.has(key))
-                                delete frame[key];
-                        }
+                                        for (var key in frame) {
+                                            if (!desiredFrameData.has(key))
+                                                delete frame[key];
+                                        }
 
-                        frame.events = frame.events.filter(function(evt) {
-                            return evt.eventType === 'ITEM_PURCHASED' || evt.eventType === 'ITEM_UNDO';
+                                        frame.events = frame.events.filter(function(evt) {
+                                            return evt.eventType === 'ITEM_PURCHASED' || evt.eventType === 'ITEM_UNDO';
+                                        });
+                                        return frame.events.length !== 0;
+                                    });
+                                    for (var key in matchData.timeline) {
+                                        if (!desiredTimelineData.has(key))
+                                            delete matchData.timeline[key];
+                                    }
+
+                                    matchData.participants.forEach(function(participant) {
+                                        if (participant.championId === '126') console.log('yay!');
+                                        for (var key in participant) {
+                                            if (!desiredParticipantData.has(key))
+                                                delete participant[key];
+                                        }
+                                    });
+
+                                    for (var key in matchData) {
+                                        if (!desiredData.has(key)) {
+                                            delete matchData[key];
+                                        }
+                                    }
+
+                                    db.collection('matches' + MODE).insert(matchData);
+                                });
                         });
-                        return frame.events.length !== 0;
-                    });
-                    for (var key in matchData.timeline) {
-                        if (!desiredTimelineData.has(key))
-                            delete matchData.timeline[key];
-                    }
-
-                    matchData.participants.forEach(function(participant) {
-                        if (participant.championId === '126') console.log('yay!');
-                        for (var key in participant) {
-                            if (!desiredParticipantData.has(key))
-                                delete participant[key];
-                        }
-                    });
-
-                    for (var key in matchData) {
-                        if (!desiredData.has(key)) {
-                            delete matchData[key];
-                        }
-                    }
-
-                    db.collection('matches' + MODE).insert(matchData);
-                });
-        })
-        .catch(console.error)
-        .then(function() { db.close(); });
+                }))
+                .catch(console.error)
+                .then(function() { db.close(); });
+        });
 }
 
 fetchAndStore();
