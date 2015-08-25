f1a0f6eba11420281775e647c335248a74d95a54
Bryan Jensen <bryanawjensen@gmail.com>
2015-08-24 19:13:34 -0700

    General cleanup, removing angulartics, adding custom code, removing auto GA pageview send

--- web-server/js/angular_app.js
+++ web-server/js/angular_app.js
@@ -4,39 +4,36 @@
 
 var buildTrees = angular.module('buildTrees', [
   'ngRoute',
-  'ngResource',
-  'angulartics',
-  'angulartics.google.analytics'
+  'ngResource'
 ]);
 
 buildTrees.factory('ChampDataEntry', function($resource, $q, $routeParams) {
-    return function(mode) {
-      return $q.all({
-        data: $resource('data/' + $routeParams.champName + mode + '.json', {}, {}).get().$promise,
-        item: $resource('data/item' + mode + '.json', {}, {}).get().$promise,
-        champ: $resource('data/champ' + mode + '.json', {}, {}).get().$promise
-      });
-    };
-  }
-)
+  return function(mode) {
+    return $q.all({
+      data: $resource('data/' + $routeParams.champName + mode + '.json', {}, {}).get().$promise,
+      item: $resource('data/item' + mode + '.json', {}, {}).get().$promise,
+      champ: $resource('data/champ' + mode + '.json', {}, {}).get().$promise
+    });
+  };
+});
+
+buildTrees.controller('MainCtrl', function($scope, $location, $window) {
+  $scope.$on('$viewContentLoaded', function(event) {
+    $window.ga('send', 'pageview', { page: $location.url() });
+  });
+});
 
 buildTrees.controller('ChampDataCtrl', function($scope, ChampDataEntry) {
-    ChampDataEntry('After').then(function(result) {
-        $('#after-container .loading-spinner').hide();
-        plot(result.data, result.item, result.champ, '#after-container', false);
-      });
+  ChampDataEntry('After').then(function(result) {
+      $('#after-container .loading-spinner').hide();
+      plot(result.data, result.item, result.champ, '#after-container', false);
+    });
 
-    ChampDataEntry('Before').then(function(result) {
-        $('#before-container .loading-spinner').hide();
-        plot(result.data, result.item, result.champ, '#before-container', true);
-      });
-
-    // ChampDataEntry.get().$promise.then(function(data) {
-    //   console.log(data);
-    // });
-    // console.log($scope.result.data);
-
-  });
+  ChampDataEntry('Before').then(function(result) {
+      $('#before-container .loading-spinner').hide();
+      plot(result.data, result.item, result.champ, '#before-container', true);
+    });
+});
 
 buildTrees.config(['$routeProvider',
   function($routeProvider) {
