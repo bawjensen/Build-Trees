a75541d842783bdf9660a723dadb3ed45e204aaa
Bryan Jensen <bryanawjensen@gmail.com>
2015-08-27 17:42:06 -0700

    Forcing input to lose focus after searching is done

--- web-server/js/script.js
+++ web-server/js/script.js
@@ -1,16 +1,23 @@
 $(function() {
+    function navigateTo(newHash) {
+        window.location.hash = newHash;
+        $('#search').blur();
+    }
+
     $('#search').keyup(function(evt) {
         if (evt.keyCode === 13) { // Hit 'enter' in the search field
             var champName = $(this).val().toLowerCase();
             if (champNameList[champName]) { // If the input is a valid champion name
-                window.location.hash = '/' + champName; // Redirect to that page
+                // window.location.hash = '/' + champName; // Redirect to that page
+                navigateTo('/' + champName); // Redirect to that page
             }
             else {
                 var regexMatch = new RegExp('^' + champName, 'i');
                 var possibilities = $('datalist#champions option').filter(function() { return this.value.match(regexMatch); });
                 if (possibilities.length) { // If the input is a partial match
                     $(possibilities[9]).click();
-                    window.location.hash = '/' + possibilities[0].value.toLowerCase(); // Redirect to the first partial match
+                    // window.location.hash = '/' + possibilities[0].value.toLowerCase(); // Redirect to the first partial match
+                    navigateTo('/' + possibilities[0].value.toLowerCase()); // Redirect to the first partial match
                 }
                 else {
                     alert('"' + champName + '" wasn\'t a valid champion name search'); // Else yell at them
