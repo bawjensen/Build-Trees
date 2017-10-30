440cbd3f81d97ba720650389e20d0bcc2220cdeb
Bryan Jensen <bryan@thumbtack.com>
2017-10-29 21:22:33 -0700

    Remove MDL styles for header in preference of custom styles
    This is because the MDL layout header styles hide
    the thing on mobile screens. I can't figure out why they
    opted for that, so I decided to just write the behavior
    I wanted.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './Header.css';
 import Link from '../Link';
 
 class Header extends React.Component {
   render() {
     return (
-      <header
-        className="mdl-layout__header mdl-layout__header--transparent mdl-layout__header--scroll"
-      >
-        <div className="mdl-layout__header-row">
-          <span className="mdl-layout__title">
-            <Link className="mdl-button mdl-js-button mdl-button--colored mdl-button--raised mdl-js-ripple-effect" to="/">
-              Build Trees
-            </Link>
-          </span>
+      <header className={s.root}>
+        <div className="mdl-grid">
+          <div className="mdl-cell mdl-cell--12-col">
+            <span className="mdl-layout__title">
+              <Link className="mdl-button mdl-js-button mdl-button--colored mdl-button--raised mdl-js-ripple-effect" to="/">
+                Build Trees
+              </Link>
+            </span>
+          </div>
         </div>
       </header>
     );
   }
 }
 
-export default Header;
+export default withStyles(s)(Header);
